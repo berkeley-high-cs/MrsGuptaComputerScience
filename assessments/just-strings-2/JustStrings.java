fd6b729a4d3aab387d123e09af7e35cc7ddea675
MrsGuptaComputerScience <48163797+MrsGuptaComputerScience@users.noreply.github.com>
2024-05-19 15:08:45 -0400

    Updating assessments/just-strings-2/JustStrings.java

--- assessments/just-strings-2/JustStrings.java
+++ assessments/just-strings-2/JustStrings.java
@@ -8,6 +8,7 @@ public class JustStrings {
   public String insert(String first, String second, int index)
   {
     String left = first.substring(0, index);
+    if (index + second.length() < first.length)
     String leftover = first.substring( index + second.length() );
     return left + second + leftover;
   }
