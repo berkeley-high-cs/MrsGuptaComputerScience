68493dca708ccc5eeb578bd6aa7fc5850098873c
MrsGuptaComputerScience <48163797+MrsGuptaComputerScience@users.noreply.github.com>
2024-05-19 13:49:46 -0400

    Updating assignments/counting/Counting.java

--- assignments/counting/Counting.java
+++ assignments/counting/Counting.java
@@ -9,7 +9,7 @@ public class Counting {
   public int countLetter(String str1, String str2)
   {
 int counter = 0 ; 
-for (int i = 0 ; i <= str1.length(); i++)
+for (int i = 0 ; i < str1.length(); i++)
 {
   if (str1.substring(i,i+1).equals(str2))
   counter ++;
