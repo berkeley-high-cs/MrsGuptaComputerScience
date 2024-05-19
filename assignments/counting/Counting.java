2398cbd29aa20cf47ec50c8c90a57966650bfb22
MrsGuptaComputerScience <48163797+MrsGuptaComputerScience@users.noreply.github.com>
2024-05-19 13:47:30 -0400

    Updating assignments/counting/Counting.java

--- assignments/counting/Counting.java
+++ assignments/counting/Counting.java
@@ -6,5 +6,19 @@ public class Counting {
   public boolean isVowel(String c) {
     return "aeiou".indexOf(c) != -1;
   }
+  public int countLetter(String str1, String str2)
+  {
+int counter = 0 ; 
+for (int i = 0 ; i < str1.length(); i++)
+{
+  if (str1.substring(i,i+1).equals(str2))
+  counter ++;
+  
+}
+
+return counter; 
+
+
+  }
 
 }
