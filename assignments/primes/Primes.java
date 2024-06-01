182d803362e2d0d25f83c94be137e21df76eb7ce
MrsGuptaComputerScience <48163797+MrsGuptaComputerScience@users.noreply.github.com>
2024-06-01 10:03:16 -0400

    Updating assignments/primes/Primes.java

--- assignments/primes/Primes.java
+++ assignments/primes/Primes.java
@@ -15,6 +15,8 @@ public class Primes {
   {
     for (int i = 2 ; i < number; i++)
     {
+      if (number == 1)
+      return false; 
       if (number == 2)
       return true; 
       if( number % i == 0)
